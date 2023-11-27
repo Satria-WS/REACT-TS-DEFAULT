b36ceebea2135d9d219b91f7468f485f06e2c279
Satria-WS <extencezone02@gmail.com>
2023-11-27 08:41:56 +0700

    change hello world deploy ci

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const App: React.FC = () => {
   return (
     <div className="App">
-      <h1>Hello Wooooorld</h1>
+      <h1>Hello Wooooorld so difficult</h1>
     </div>
   );
 };
